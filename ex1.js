0e401485e45e547892ef936be86a3044a93c72c4
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-07 08:21:59 -0800

    working on ex3

---
+++ ex1.js
@@ -0,0 +1,9 @@
+//use the parrots collection to find all documents where age is greater
+//then the first arg in the script
+
+var mongo = require('mongodb').MongoClient
+
+mongo.connect("mongodb://localhost:27017/learnyoumongo",function(err, db){
+
+} 
+
