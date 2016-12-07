f0a44886d44c692d3d2198c5972a5a439fbb4b69
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-07 08:29:16 -0800

    finished the first ex

--- ex1.js
+++ ex1.js
@@ -4,6 +4,15 @@
 var mongo = require('mongodb').MongoClient
 
 mongo.connect("mongodb://localhost:27017/learnyoumongo",function(err, db){
-
-} 
+	if(err) throw err;
+	
+	var parrots = db.collection('parrots')//specifying the collection
+	parrots.find({
+		age: {$gt: + process.argv[2]}//querying the collection 
+		}).toArray(function(err, docs){//converting results to arr
+			if(err) throw err;
+			console.log(docs)//print results
+			db.close();//close the db
+		})
+}) 
 
